fc06ade286c9981054e5cb518f95201d4b9aff67
Franklin <antoniofranklin576@gmail.com>
2023-12-30 10:48:41 -0300

    Imagens e descrições estão correspondentes a escolha do personagem

--- src/js/main.js
+++ src/js/main.js
@@ -8,12 +8,16 @@ buttons.forEach((btn, indice) => {
         removeClassCharacter()
 
         btn.classList.add("selected")
-        btn.querySelector("img").getAttribute("src").substring(15).split(".jpg")[0]
         
-        characters[indice].querySelector("img").getAttribute("src").substring(20).split(".png")[0]
-
-
-        characters[indice].classList.add("selected")
+        let imageBtnName = btn.querySelector("img").getAttribute("src")
+        .substring(15).split(".jpg")[0]
+        
+        let imageCharacterName = characters[indice].querySelector("img").getAttribute("src")
+        .substring(20).split(".png")[0]
+        
+        imageBtnName !== imageCharacterName ? findImageCharacter(imageBtnName) : characters[indice].classList.add("selected")
+        
+        
     })
 })
 
@@ -25,4 +29,11 @@ function removeClassBtn(){
 function removeClassCharacter(){
     const characterSelected = document.querySelector(".character.selected")
     characterSelected.classList.remove("selected")
+}
+
+function findImageCharacter(name){
+    characters.forEach((character, ind) => {
+        let x = character.querySelector("img").getAttribute("src").substring(20).split(".png")[0]
+        if (x == name) characters[ind].classList.add("selected")
+    })
 }

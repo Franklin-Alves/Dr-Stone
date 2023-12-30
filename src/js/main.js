11ddc2d45fec1591beadc0d5827cd912810f86ea
Franklin <antoniofranklin576@gmail.com>
2023-12-29 22:18:01 -0300

    Seleção dos personagens dinâmica

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
-let charcterSelected = 
+const characters = document.querySelectorAll(".character")
+const buttons = document.querySelectorAll(".btn")
+
+buttons.forEach((btn, indice) => {
+    btn.addEventListener("click", () => {
+
+        removeClassBtn()
+        removeClassCharacter()
+
+        btn.classList.add("selected")
+        btn.querySelector("img").getAttribute("src").substring(15).split(".jpg")[0]
+        
+        characters[indice].querySelector("img").getAttribute("src").substring(20).split(".png")[0]
+
+
+        characters[indice].classList.add("selected")
+    })
+})
+
+function removeClassBtn(){
+    const buttonSelected = document.querySelector(".btn.selected")
+    buttonSelected.classList.remove("selected")
+}
+
+function removeClassCharacter(){
+    const characterSelected = document.querySelector(".character.selected")
+    characterSelected.classList.remove("selected")
+}
